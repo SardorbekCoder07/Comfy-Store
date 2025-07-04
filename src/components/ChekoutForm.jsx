5185b71270b6303021c9624d91b59439330455a7
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2025-07-04 10:33:32 +0500

    order request setup

--- src/components/ChekoutForm.jsx
+++ src/components/ChekoutForm.jsx
@@ -1,7 +1,60 @@
-const ChekoutForm = () => {
+import { Form, redirect } from 'react-router'
+import FormInput from './FormInput'
+import SubmitBtn from './SubmitBtn'
+import { customFetch, formatPrice } from '../utils'
+import { toast } from 'sonner'
+import { clearCart } from '../features/cart/cartSlice'
+
+export const action =
+  (store) =>
+    async ({ request }) => {
+      const formData = await request.formData()
+      const { name, address } = Object.fromEntries(formData)
+      const user = store.getState().userState.user
+      const { cartItems, orderTotal, numItemsInCart } =
+        store.getState().cartState
+
+      const info = {
+        name,
+        address,
+        chargeTotal: orderTotal,
+        orderTotal: formatPrice(orderTotal),
+        cartItems,
+        numItemsInCart,
+      }
+
+      try {
+        const response = await customFetch.post(
+          '/orders',
+          { data: info },
+          {
+            headers: {
+              Authorization: `Bearer ${user.token}`,
+            },
+          }
+        )
+        store.dispatch(clearCart())
+        toast.success('order placed successfully')
+        return redirect('/orders')
+      } catch (error) {
+        console.log(error)
+        const errorMessage = error?.response?.data?.error?.message || 'there was an error placing your order '
+        toast.error(errorMessage)
+        if(error.response.status===401) return redirect('/login')
+        return null
+      }
+    }
+
+const CheckoutForm = () => {
   return (
-	<div>ChekoutForm</div>
+    <Form method='POST' className='flex flex-col gap-y-4'>
+      <h4 className='font-medium text-xl capitalize'>shipping information</h4>
+      <FormInput label='first name' name='name' type='text' />
+      <FormInput label='address' name='address' type='text' />
+      <div className='mt-4'>
+        <SubmitBtn text='place your order' />
+      </div>
+    </Form>
   )
 }
-
-export default ChekoutForm
+export default CheckoutForm
